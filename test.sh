193eae6fadf451bd8138294f6e55af3908044a6d
D. A. Pugson <dougalpugson@gmail.com>
2021-10-17 07:32:57 -0400

    Delete test.sh
    Remove unneeded shell script.

--- test.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch --env-filter 'if [ true ]; then
-     GIT_AUTHOR_EMAIL=dougal.pugson@gmail.com;
-     GIT_AUTHOR_NAME="Dougal Pugson";
-     GIT_COMMITTER_EMAIL=dougal.pugson@gmail.com;
-     GIT_COMMITTER_NAME="Dougal Pugson"; fi' -- --all
-
